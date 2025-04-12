833b48de531dee55158b0304c11cdb72e7f060a3
yuvarajmck <yuvaraj.m@terzocloud.com>
2025-04-12 21:51:45 +0530

    pdf extraction fix => v4.1.3

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the Angular app
-app.use(express.static(path.join(__dirname, 'dist/ocr-tool')));
+app.use(express.static(__dirname + '/dist/ocr-tool'));
 
 // All other routes should redirect to the index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/ocr-tool/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ocr-tool/index.html'));
 });
 
 app.listen(PORT, () => {
